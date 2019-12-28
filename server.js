b01bdca500f246349b41ad05a115c8604be84cb2
jgodin <jakegod@live.ca>
2019-12-28 18:20:19 -0500

    Implement authentication

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect db
+connectDB();
+
+// Init Middleware
+
+//Body Parser
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => {
     res.send('Hello world!');
 });
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 const PORT = process.env.PORT || 5000;
 
